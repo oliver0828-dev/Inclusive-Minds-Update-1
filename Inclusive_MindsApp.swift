7ccbe125f17ca3a1aec6b95021d27c703390ff43
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2023-11-21 12:16:02 +0900

    Add files via upload

---
+++ Inclusive_MindsApp.swift
@@ -0,0 +1,18 @@
+//
+//  Inclusive_MindsApp.swift
+//  Inclusive Minds
+//
+//  Created by Oliver Park on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Inclusive_MindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
